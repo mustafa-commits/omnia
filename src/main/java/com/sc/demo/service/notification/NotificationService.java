63136f58b5eb2c3b5fc487291b4b69c6fc17cad0
Omnia <o.m.a.1514.ja@gmail.com>
2026-04-07 21:36:44 +0300

    fixed endpoint names

--- src/main/java/com/sc/demo/service/notification/NotificationService.java
+++ src/main/java/com/sc/demo/service/notification/NotificationService.java
@@ -3,18 +3,14 @@ package com.sc.demo.service.notification;
 import com.sc.demo.model.notification.Notification;
 import com.sc.demo.model.notification.NotificationDetails;
 import com.sc.demo.model.users.dto.NotificationRequest;
-import com.sc.demo.model.users.dto.NotificationResponse;
-import com.sc.demo.model.users.dto.PrivateNotification;
-import com.sc.demo.model.users.dto.PublicNotification;
-import com.sc.demo.repository.AppUserRepo;
+import com.sc.demo.model.users.dto.NotificationByType;
+import com.sc.demo.model.users.dto.PHoneNotification;
 import com.sc.demo.repository.NotificationDetailsRepo;
 import com.sc.demo.repository.NotificationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,52 +26,50 @@ public class NotificationService {
     @Autowired
     private NotificationDetailsRepo notificationDetailsRepo;
 
-    public Notification notification1(long notification_id) {
-        Optional<Notification> byId = notificationRepo.findById(notification_id);
+//    public Notification notification1(long notification_id) {
+//        Optional<Notification> byId = notificationRepo.findById(notification_id);
+//
+//        if (byId.isPresent()) {
+//            return byId.get();
+//        } else
+//            return null;
+//
+//    }
 
-        if (byId.isPresent()) {
-            return byId.get();
-        } else
-            return null;
-
-    }
-
+    // انشاء اشعار
     public Notification createNotification(NotificationRequest notificationRequest){
-
         Notification notification = new Notification(notificationRequest.sendId(),
                 notificationRequest.title(), notificationRequest.description(),
                  notificationRequest.notificationType()
         );
-
         notification= notificationRepo.save(notification);
-
         for (NotificationDetails n :notificationRequest.notificationDetails()){
             notificationDetailsRepo.save(new NotificationDetails(n.getUser_id(),notification));
         }
-
-
         return notification;
     }
 
-    public NotificationResponse notification(long user_id) {
+//    public NotificationResponse notification(long user_id) {
+//
+//        NotificationResponse NoteCome = jdbcClient.sql("""
+//                    select N.TITLE, N.DESCRIPTION, N.send_id
+//                    from SC_NOTIFICATION N
+//                        join SC_NOTIFICATION_DETAILS ND on N.NOTIFICATION_ID = ND.NOTIFICATION_ID
+//                        where user_id = :user_id
+//                """).param("user_id",user_id).query(NotificationResponse.class).single();
+//
+//        return NoteCome;
+//    }
 
-        NotificationResponse NoteCome = jdbcClient.sql("""
-                    select N.TITLE, N.DESCRIPTION, N.send_id
-                    from SC_NOTIFICATION N
-                        join SC_NOTIFICATION_DETAILS ND on N.NOTIFICATION_ID = ND.NOTIFICATION_ID where user_id = :user_id
-                """).param("user_id",user_id).query(NotificationResponse.class).single();
+    // اشعارات التطيق لكل يززر
+    public PHoneNotification PHoneNotification(long user_id) {
 
-        return NoteCome;
-    }
-
-    public PublicNotification PublicNotification(long user_id) {
-
-        Optional <PublicNotification>  pNote = jdbcClient.sql("""
+        Optional <PHoneNotification>  pNote = jdbcClient.sql("""
                    select n.CREATE_DATE, n.TITLE, n.DESCRIPTION
                    from SC_NOTIFICATION n
                    left join SC_NOTIFICATION_DETAILS nd on n.NOTIFICATION_ID = nd.NOTIFICATION_ID
                    Where ND.user_id = :user_id
-                """).param("user_id",user_id).query(PublicNotification.class).optional();
+                """).param("user_id",user_id).query(PHoneNotification.class).optional();
 
         if (pNote.isPresent())
             return pNote.get();
@@ -83,14 +77,13 @@ public class NotificationService {
                 return null;
     }
 
-    public PrivateNotification PrivateNotification(long notification_type) {
-
-        Optional <PrivateNotification> pNote = jdbcClient.sql("""
+    // جلب اشعارات الداشبورد حسب النوع (عامة او خاصة)
+    public NotificationByType NotificationByType(long notification_type) {
+        Optional <NotificationByType> pNote = jdbcClient.sql("""
                    SELECT SEND_ID, TITLE, DESCRIPTION, CREATE_DATE
                    FROM SC_NOTIFICATION
                    where notification_type = :notification_type;
-                
-                """).param("notification_type",notification_type).query(PrivateNotification.class).optional();
+                """).param("notification_type",notification_type).query(NotificationByType.class).optional();
 
         if (pNote.isPresent())
             return pNote.get();
