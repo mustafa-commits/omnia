6f08cd10fbdab7fb5314f552ffce72f9cf2bd64f
mustafa <mustafa.zuhair@aynyateem.com>
2026-05-31 12:57:17 +0300

    tt

--- src/main/java/com/sc/demo/controller/AddPhoneController.java
+++ src/main/java/com/sc/demo/controller/AddPhoneController.java
@@ -1,5 +1,6 @@
 package com.sc.demo.controller;
 
+import com.sc.demo.SecuredRestController;
 import com.sc.demo.config.OpenApi30Config;
 import com.sc.demo.model.dto.AddPhone.CheckPhone;
 import com.sc.demo.service.AddPhone.AddPhoneService;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AddPhoneController implements OpenApi30Config {
+public class AddPhoneController implements SecuredRestController {
 
     @Autowired
     private AddPhoneService addPhoneService;
