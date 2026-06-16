fe617aac3c1f05147f8e3934ac40125d28be9a42
Omnia <amnia.adnan@aynyateem.com>
2026-06-16 13:37:42 +0300

    add phone type

--- src/main/java/com/sc/demo/service/login/LoginService.java
+++ src/main/java/com/sc/demo/service/login/LoginService.java
@@ -15,8 +15,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/sc/demo/service/users/FamilyInfoService.java
+++ src/main/java/com/sc/demo/service/users/FamilyInfoService.java
@@ -4,15 +4,12 @@ import com.sc.demo.model.dto.familyInfo.ChildrenAndMailyFamilyMembersResponse;
 import com.sc.demo.model.dto.familyInfo.FamilyInfoBasicResponse;
 import com.sc.demo.model.dto.familyInfo.FamilyInfoHousingResponse;
 import com.sc.demo.model.users.PhoneType;
-import com.sc.demo.model.users.Token;
 import com.sc.demo.model.verification.VerificationApp;
 import com.sc.demo.repository.familyInfo.FamilyInfoRepo;
 import com.sc.demo.service.token.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,16 +31,7 @@ public class FamilyInfoService {
         var headFamilyId = tokenService.decodeToken(token.substring(7)).getClaim("headFamilyId");
         var requestId = tokenService.decodeToken(token.substring(7)).getClaim("requestId");
 
-        Optional<VerificationApp> byHeadFamilyId = familyInfoRepo.findById(Long.parseLong(headFamilyId.toString()));
-        System.out.println(byHeadFamilyId);
-        Optional<VerificationApp> byRequestId = familyInfoRepo.findById(Long.parseLong(requestId.toString()));
-        System.out.println(byRequestId);
-
-        if (byHeadFamilyId.isPresent() && byRequestId.isPresent()) {
-            VerificationApp newInfo = byHeadFamilyId.get();
-            newInfo.setPhoneType(phoneType);
-            familyInfoRepo.save(newInfo);
-        }
+        familyInfoRepo.save(new VerificationApp(phoneType));
 
         return jdbcClient.sql("""
                     SELECT F.OLD_FAMILY_NO AS FamilyNo
