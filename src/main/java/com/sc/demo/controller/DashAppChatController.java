932d90e9fd35fc8c7a2ceb18645bde547ea3ce77
Omnia <amnia.adnan@aynyateem.com>
2026-06-16 09:21:07 +0300

    change Chat Activity endpoint

--- src/main/java/com/sc/demo/controller/DashAppChatController.java
+++ src/main/java/com/sc/demo/controller/DashAppChatController.java
@@ -1,15 +1,9 @@
 package com.sc.demo.controller;
 
-import com.sc.demo.model.chat.MsgType;
 import com.sc.demo.model.dto.chat.*;
 import com.sc.demo.service.chat.DashAppChatService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -18,18 +12,6 @@ public class DashAppChatController {
 
     @Autowired
     private DashAppChatService dashAppChatService;
-
-    @Value("${ATTACHMENT_PATH_CHAT}")
-    private String uploadDir;
-
-    @Value("${ATTACHMENT_PATH_VOICE}")
-    private String uploadVoice;
-
-    // تخزين Token
-    @PostMapping("/V1/api/sc/setDashChatToken")
-    public long setChatToken(@RequestBody ChatTokenRequest chatTokenRequest){
-        return dashAppChatService.saveToken(chatTokenRequest);
-    }
 
     // اظهار المحادثات الفعالة
     @GetMapping("/V1/api/sc/getDashPhoneChats")
@@ -43,49 +25,10 @@ public class DashAppChatController {
         return dashAppChatService.dashPhoneChatsArchived();
     }
 
-//    // ارسال رسالة
-//    @PostMapping("/V1/api/sc/dashWriteMessages")
-//    public boolean dashWriteMessages(@RequestParam Long chatId,
-    //                                 @RequestParam Platform platform,
-    //                                 @RequestParam String messages,
-    //                                 @RequestParam MsgType msgType,
-    //                                 @RequestParam Long createBy,
-    //                                 @RequestParam(required = false) MultipartFile file,
-    //                                 @RequestParam(required = false) MultipartFile voice,
-    //                                 @RequestHeader(name = "authorization") String token){
-//        return dashAppChatService.dashWriteMessages(new MessagesRequest(chatId,
-//                platform, messages, msgType, createBy), file, voice, token);
-//    }
-//
-    //  اظهار الرسائل في المحادث في الداشبورد
-    @GetMapping("/V1/api/sc/dashGetMessagesChat")
-    public List<MessagesResponse> dashGetMessagesChat(@RequestParam long chatId){
-        return dashAppChatService.dashGetMessages(chatId);
-    }
-
-    // اظهار الصور والصوت
-    @GetMapping("/V1/api/sc/dashPhotoChat/{filename:.+}")
-    public void serveFile(
-            @PathVariable String filename,
-            @RequestParam MsgType msgType,
-            HttpServletResponse response
-    ) throws IOException {
-        var file = Paths.get(uploadDir, filename);
-        var voice = Paths.get(uploadVoice, filename);
-        System.out.println("Looking for file at: " + file.toAbsolutePath());
-        if (Files.exists(file) && msgType == MsgType.IMAGE) {
-            response.setContentType(Files.probeContentType(file));
-            response.setContentLengthLong(Files.size(file));
-            Files.copy(file, response.getOutputStream());
-            return;
-        }
-        if (Files.exists(voice) && msgType == MsgType.VOICE){
-            response.setContentType(Files.probeContentType(voice));
-            response.setContentLengthLong(Files.size(voice));
-            Files.copy(voice, response.getOutputStream());
-            return;
-        }
-        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    // تحويل المحادثات من فعالة الى مؤرشفة
+    @PutMapping("/V1/api/sc/changeChatActivity")
+    public Boolean changeChatActivity(){
+        return dashAppChatService.changeChatActivity();
     }
 }
 
--- src/main/java/com/sc/demo/service/chat/DashAppChatService.java
+++ src/main/java/com/sc/demo/service/chat/DashAppChatService.java
@@ -1,64 +1,16 @@
 package com.sc.demo.service.chat;
 
-import com.sc.demo.model.chat.AppChatDetails;
-import com.sc.demo.model.chat.MsgType;
-import com.sc.demo.model.chat.Platform;
-import com.sc.demo.model.chat.WhoIsSender;
 import com.sc.demo.model.dto.chat.*;
-import com.sc.demo.model.users.Token;
-import com.sc.demo.repository.chat.ChatRepo;
-import com.sc.demo.repository.chat.ChatTokenRepo;
-import com.sc.demo.repository.chat.MessagesRepo;
-import com.sc.demo.service.token.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DashAppChatService {
 
     @Autowired
-    private ChatTokenRepo chatTokenRepo;
-
-    @Autowired
     private JdbcClient jdbcClient;
-
-    @Autowired
-    private TokenService tokenService;
-
-    @Autowired
-    private Environment environment;
-
-    @Autowired
-    private ChatRepo chatRepo;
-
-    @Autowired
-    private MessagesRepo messagesRepo;
-
-    // حفظ Token القادم من firebase في قاعدة البيانات
-    public long saveToken(ChatTokenRequest chatTokenRequest) {
-        Optional<Token> byToken = chatTokenRepo.findById(chatTokenRequest.userId());
-
-        if (byToken.isPresent()) {
-            Token token = byToken.get();
-            token.setLastUpdate(LocalDateTime.now());
-            token.setToken(chatTokenRequest.token());
-            return chatTokenRepo.save(token).getUserId();
-        } else {
-            Token token = new Token();
-            token.setToken(chatTokenRequest.token());
-            token.setUserId(chatTokenRequest.userId());
-            return chatTokenRepo.save(token).getUserId();
-        }
-    }
 
     // جلب المحادثات المفعلة
     public List<DashAppChatResponse> dashPhoneChats(){
@@ -96,63 +48,8 @@ public class DashAppChatService {
                 .list();
     }
 
-    public boolean dashWriteMessages(MessagesRequest messagesRequest, MultipartFile file, MultipartFile voice, String token) {
-        var userTokenId = tokenService.decodeToken(token.substring(7)).getSubject();
-        String newFileName = null;
-
-        if(messagesRequest.msgType() == MsgType.IMAGE) {
-            try {
-                String originalFileName = file.getOriginalFilename();
-                newFileName = System.nanoTime() + originalFileName.substring(originalFileName.lastIndexOf("."));
-                String filePath = environment.getProperty("ATTACHMENT_PATH_CHAT") + newFileName;
-                file.transferTo(new File(filePath));
-                System.out.println(filePath);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        if(messagesRequest.msgType() == MsgType.VOICE) {
-            try {
-                String originalVoiceName = voice.getOriginalFilename();
-                newFileName = System.nanoTime() + originalVoiceName.substring(originalVoiceName.lastIndexOf("."));
-                String filePath = environment.getProperty("ATTACHMENT_PATH_VOICE") + newFileName;
-                voice.transferTo(new File(filePath));
-                System.out.println(filePath);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        WhoIsSender whoIsSender = Platform.APP.equals(messagesRequest.platform())
-                ? WhoIsSender.USER
-                : WhoIsSender.EMPLOYEE;
-
-        AppChatDetails appChatDetails = new AppChatDetails(chatRepo.getReferenceById(messagesRequest.chatId()),
-                Long.parseLong(userTokenId), whoIsSender,
-                messagesRequest.platform(), messagesRequest.messages().isEmpty() ? newFileName : messagesRequest.messages(),
-                messagesRequest.msgType());
-        Long detailsChatId = messagesRepo.save(appChatDetails).getDetailsChatId();
-        System.out.println(detailsChatId);
+    public Boolean changeChatActivity(){
         return true;
     }
 
-
-    public List<MessagesResponse> dashGetMessages(long chatId){
-        System.out.println(chatId);
-        return jdbcClient.sql("""
-                SELECT CASE WHEN MSG_TYPE in (1,2) THEN TO_CHAR(:path) || MESSAGES
-                        ELSE MESSAGES END AS messages,
-                        WHO_IS_SENDER AS whoIsSender,
-                        CREATE_BY AS createBy,
-                        CREATE_DATE AS createDate
-                FROM MOBAPP.SC_CHAT_DETAILS
-                WHERE CHAT_ID = :chatId
-                order by CREATE_DATE desc
-                """)
-                .param("chatId", chatId)
-                .param("path", "http://37.239.42.53:1801/socialCare/V1/api/sc/dashPhotoChat/")
-                .query(MessagesResponse.class)
-                .list();
-    }
 }
