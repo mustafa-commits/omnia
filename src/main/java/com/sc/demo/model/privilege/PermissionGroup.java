c8b6a4585ca6d9f6e233e85ce6b26225399d316e
mustafa <mustafa.zuhair@aynyateem.com>
2026-06-27 14:59:04 +0300

    mustafa

--- src/main/java/com/sc/demo/model/privilege/PermissionGroup.java
+++ src/main/java/com/sc/demo/model/privilege/PermissionGroup.java
@@ -22,8 +22,8 @@ public class PermissionGroup {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long groupId;
 
-    @OneToMany(mappedBy = "permissionGroup", cascade = CascadeType.ALL)
-    private List<Permissions> privilegeGroupId = new ArrayList<>();
+//    @OneToMany(mappedBy = "permissionGroup", cascade = CascadeType.ALL)
+//    private List<Permissions> privilegeGroupId = new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "groupId")
