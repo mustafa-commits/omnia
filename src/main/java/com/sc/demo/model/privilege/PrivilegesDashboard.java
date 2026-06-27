c87b87339cf6e923c0b44823077beef8bc7b8f97
Omnia <amnia.adnan@aynyateem.com>
2026-06-27 14:41:03 +0300

    update

--- src/main/java/com/sc/demo/model/privilege/PrivilegesDashboard.java
+++ src/main/java/com/sc/demo/model/privilege/PrivilegesDashboard.java
@@ -1,12 +1,10 @@
 package com.sc.demo.model.privilege;
 
-import com.sc.demo.model.employees.DashboardUsers;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
 
 @Entity
